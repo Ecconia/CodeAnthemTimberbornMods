8c0b58304e613dca4d0ed7319a20b44697c53c4e
Ecconia <public-commit@ecconia.com>
2024-06-13 22:21:08 +0200

    Fix MenuEntries for Update-5
    Updates the mod to version 1.1.1. Updates some references.
    Also update some links to point to Ecconias repository.
    
    Fixes that OptionBox was split for Game & MapEditor.

--- TB_CameraTweaker/KsHelperLib/UI/Menu/OptionsPatches.cs
+++ TB_CameraTweaker/KsHelperLib/UI/Menu/OptionsPatches.cs
@@ -1,27 +1,34 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using TB_CameraTweaker.KsHelperLib.Localization;
 using TimberApi.DependencyContainerSystem;
+using TimberApi.UiBuilderSystem.CustomElements;
 using Timberborn.Localization;
 using Timberborn.MainMenuScene;
-using Timberborn.Options;
+using Timberborn.MapEditorUI;
+using Timberborn.OptionsGame;
 using UnityEngine.UIElements;
 
 namespace TB_CameraTweaker.KsHelperLib.UI.Menu
 {
     internal class OptionsPatches
     {
-        [HarmonyPatch(typeof(OptionsBox), "GetPanel")]
+        [HarmonyPatch(typeof(GameOptionsBox), "GetPanel")]
         public static class InGameMenuPanelPatch
         {
             public static void Postfix(ref VisualElement __result) {
                 Plugin.Log.LogDebug("Menu Patch: InGame");
                 VisualElement root = __result.Query("OptionsBox");
-                Button button = new() {
-                    classList = { "menu-button" },
-                    text = DependencyContainer.GetInstance<ILoc>().T($"{LocConfig.LocTag}.menu.title")
-                };
-                button.clicked += OptionsMenu.OpenOptionsDelegate;
-                root.Insert(6, button);
+                MenuButtonInjector.InjectMenuButtonAt(root, 6);
+            }
+        }
+
+        [HarmonyPatch(typeof(MapEditorOptionsBox), "GetPanel")]
+        public static class InMapEditorMenuPanelPatch
+        {
+            public static void Postfix(ref VisualElement __result) {
+                Plugin.Log.LogDebug("Menu Patch: MapEditor");
+                VisualElement root = __result.Query("OptionsBox");
+                MenuButtonInjector.InjectMenuButtonAt(root, 6);
             }
         }
 
@@ -34,12 +41,22 @@ namespace TB_CameraTweaker.KsHelperLib.UI.Menu
             public static void Postfix(ref VisualElement __result) {
                 Plugin.Log.LogDebug("Menu Patch: MainMenu");
                 VisualElement root = __result.Query("MainMenuPanel");
-                Button button = new() {
-                    classList = { "menu-button" },
-                    text = DependencyContainer.GetInstance<ILoc>().T($"{LocConfig.LocTag}.menu.title")
+                MenuButtonInjector.InjectMenuButtonAt(root, 6);
+            }
+        }
+
+        private static class MenuButtonInjector
+        {
+            public static void InjectMenuButtonAt(VisualElement root, int index)
+            {
+                LocalizableButton button = new() {
+                    name = "CameraTweaker",
+                    classList = { "menu-button", "menu-button--stretched" },
+                    //Automatic localization by key does not work. Manually set the text:
+                    text = DependencyContainer.GetInstance<ILoc>().T($"{LocConfig.LocTag}.menu.title"),
                 };
                 button.clicked += OptionsMenu.OpenOptionsDelegate;
-                root.Insert(6, button);
+                root.Insert(index, button);
             }
         }
     }
